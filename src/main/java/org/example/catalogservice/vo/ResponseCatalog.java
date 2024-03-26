9b77599dff2a98f6b37da05f584308668c84212a
space <enaenen@naver.com>
2024-03-27 02:03:20 +0900

    entity & service

--- src/main/java/org/example/catalogservice/vo/ResponseCatalog.java
+++ src/main/java/org/example/catalogservice/vo/ResponseCatalog.java
@@ -7,8 +7,10 @@ import lombok.Data;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseCatalog {
+
 	private String productId;
 	private String productName;
+	private Integer unitPrice;
 	private Integer stock;
 	private LocalDate createdAt;
 }
